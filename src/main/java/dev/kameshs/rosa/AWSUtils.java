5dd38fd7bbe8ec05d76f175a56d1bb383f4062e8
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-03-26 20:15:59 +0530

    WIP: Basic deployment

--- src/main/java/dev/kameshs/rosa/AWSUtils.java
+++ src/main/java/dev/kameshs/rosa/AWSUtils.java
@@ -1,21 +1,30 @@
 package dev.kameshs.rosa;
 
+import java.util.logging.Logger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
 import software.amazon.awssdk.auth.credentials.WebIdentityTokenFileCredentialsProvider;
-import software.amazon.awssdk.http.urlconnection.UrlConnectionHttpClient;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheSdkHttpService;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
 @ApplicationScoped
 public class AWSUtils {
 
+  private static final Logger LOGGER = Logger.getLogger(
+    AWSUtils.class.getName());
+
   @Named("WebIdProviderClient")
   public DynamoDbClient dynamoDbClient() {
+    LOGGER.info("Building DynamoDbClient with WebIdentityProvider");
+    SdkHttpClient httpClient = new ApacheSdkHttpService().createHttpClientBuilder()
+                                                         .build();
+
     WebIdentityTokenFileCredentialsProvider credentialsProvider =
       WebIdentityTokenFileCredentialsProvider.builder()
                                              .build();
     return DynamoDbClient.builder()
-                         .httpClient(UrlConnectionHttpClient.create())
+                         .httpClient(httpClient)
                          .credentialsProvider(credentialsProvider)
                          .build();
   }
