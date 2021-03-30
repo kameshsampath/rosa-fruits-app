2ab6e4bceee216b746cd98d2de7d75782b81cb94
Kamesh Sampath <kamesh.sampath@hotmail.com>
2021-03-30 14:34:29 +0530

    fix: swagger UI
    - README update
    - add AWS resources for manual steps
    - remove verbose from cmdline

--- setup/hack.sh
+++ setup/hack.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-CONTAINER_CMD=${$CONTAINER_CMD:-docker}
-ANSIBLE_RUNNER_IMAGE := quay.io/kameshsampath/rosa-fruits-app-ee
+CONTAINER_CMD=${CONTAINER_CMD:-docker}
+ANSIBLE_RUNNER_IMAGE=quay.io/kameshsampath/openshift-demos-ansible-ee
 
 if [ -f "$KUBECONFIG" ];
 then
@@ -18,5 +18,5 @@ $CONTAINER_CMD run -it  \
    -v "$CURRENT_DIR/inventory:/runner/inventory:z" \
    -v "$CURRENT_DIR/env:/runner/env:z" \
    -v "$CURRENT_DIR/.kube:/home/runner/.kube:z" \
-   --env-file "$CURRENT_DIR/container-env" \
+   --env-file "$CURRENT_DIR/.container-env" \
    "$ANSIBLE_RUNNER_IMAGE" /runner/project/run.sh
